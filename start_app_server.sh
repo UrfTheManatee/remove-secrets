a88cbe24ce260ca7cecde9ae75b6b1e633759582
Lucas Kauffman <lucas_kauffman@hotmail.com>
2018-10-03 17:27:22 +0800

    did something stupid

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://<dbuser>:<dbpassword>@ds121413.mlab.com:21413/thereisnourflevel
+export MONGODB_CREDENTIALS=mongodb://human:human1@ds121413.mlab.com:21413/thereisnourflevel
 
 # finally, run the app in dev mode...
 npm run dev
